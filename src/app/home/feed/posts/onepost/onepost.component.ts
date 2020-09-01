18d99eebc400950c657b40dbe935c66c870b180d
jagdish-cm <jagdishcm41297@gmail.com>
2020-09-02 04:56:10 +0530

    interact with one post start

---
+++ src/app/home/feed/posts/onepost/onepost.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-onepost',
+  templateUrl: './onepost.component.html',
+  styleUrls: ['./onepost.component.scss']
+})
+export class OnepostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
