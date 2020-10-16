544e7d0835dc8674acb04757fbdbb66d5446eacb
jagdish-cm <jagdishcm41297@gmail.com>
2020-10-16 09:23:10 +0530

    aa

--- src/app/assigns/assigns.component.ts
+++ src/app/assigns/assigns.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from './products.service';
 import { Product } from './product';
+import { LazyLoadEvent } from 'primeng/api';
+import { FilterMetadata } from 'primeng/api';
+
 
 // interface DataItem {
-//   title: string;
-//   subject: string;
-//   dateasgn: number;
-//   datesub: number;
+//   title: string;  filter
+//   subject: string; filter
+//   dateasgn: number; sort
+//   datesub: number; sort
 // }
 
 @Component({
@@ -15,14 +18,38 @@ import { Product } from './product';
   styleUrls: ['./assigns.component.scss']
 })
 export class AssignsComponent implements OnInit {
-    products: Product[];
+  products: Product[];
+  datasource: Product[];
+  loading: boolean;
+  totalRecords: number;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    this.productService.getProductsSmall().then(data => this.products = data);
-    };
+    this.productService.getProductsSmall().then(data => {this.datasource = data;
+      this.totalRecords = data.length;
+      console.log(this.totalRecords)});
+    this.loading = true;      
+  };
 
+    loadCustomers(event: LazyLoadEvent) {  
+      this.loading = true;
+
+      //in a real application, make a remote request to load data using state metadata from event
+      //event.first = First row offset
+      //event.rows = Number of rows per page
+      //event.sortField = Field name to sort with
+      //event.sortOrder = Sort order as number, 1 for asc and -1 for dec
+      //filters: FilterMetadata object having field as key and filter value, filter matchMode as value
+
+      //imitate db connection over a network
+      setTimeout(() => {
+        if (this.datasource) {
+          this.products = this.datasource.slice(event.first, (event.first + event.rows));
+          this.loading = false;
+      }
+      }, 1000);
+  }
     
 
 
